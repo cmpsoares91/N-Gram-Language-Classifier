a42bf7eb3f0d016694a254768320a294c7d2a290
Carlos Soares <cmpsoares91@gmail.com>
2014-10-27 22:49:22 +0000

    Algorithm ready, report missing, enhancements possible

--- nGram.py
+++ nGram.py
@@ -10,9 +10,10 @@ Created on Mon Oct 27 02:54:36 2014
 """
 
 # Imports used:
-import sys, getopt, os, re
+import sys, getopt, os, re, numpy
+import matplotlib.pyplot as plt
 from collections import Counter
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
 # Globals:
 ## Unchangeable:
@@ -27,11 +28,15 @@ testPhrase = "Olá tudo bem?\nChamo-me Carlos Soares."
 smoothing = False
 testFile = "./test.csv"
 fileToTest = False
+outFile = "./resultados.txt"
+writeOutput = False
+plot = False
 
 def normalizeText (n, text):
     temp = ''
     wordBegin = ''
     wordEnd = ''
+    text = re.sub("\d+", " ", text.lower())
     for word in re.split('\W+', text, flags=re.UNICODE):
         if word != '':
             if n == 1 :
@@ -46,19 +51,18 @@ def normalizeText (n, text):
     return temp;
 
 def normalizeFilesInDirectory (n, dir, fileExt, newExt):
-
     for file in os.listdir(dir):
         if file.endswith(fileExt):
             if os.path.splitext(file)[0] != "sources":
                 tempFile = open(dir+'/'+os.path.splitext(file)[0]+os.path.splitext(file)[1], 'r', encoding="utf8")
-                temp = re.sub("\d+", " ", tempFile.read().lower())
+                temp = tempFile.read()
                 tempFile.close()
                 
                 tempFile = open(dir+'/'+os.path.splitext(file)[0]+newExt, 'w', encoding="utf8")
                 tempFile.write(normalizeText(n, temp))
                 tempFile.close()
 
-def ngram (n, text):
+def grams (n, text):
     gram = ""
     gramArray = []
     for i in range(len(text)-n):
@@ -78,7 +82,11 @@ def ngram (n, text):
             # This is where I have to add the gram to the counter...
             gramArray.append(gram)
         gram = ""
-    count = Counter(gramArray)
+    return gramArray;
+
+def ngram (n, text):
+    
+    count = Counter(grams(n, text))
     return sorted(count.items());
             
 def createNGramFile (n, dir, ext):
@@ -97,9 +105,88 @@ def createNGramFile (n, dir, ext):
                 df.columns = [str(n) + '-gram', 'count']
 
                 df.to_csv(dir+'/'+fileName+gramExt, sep='\t', encoding='utf-8', header=False, index=False)
+                
+def div(a, b):
+    return a / b;
+    
+def getCount(gram, df):
+    try:
+        temp = df.loc[gram, 1]
+    except:
+        temp = 0
+    return temp;
 
 def testSentence (n, phrase):
     tempPhrase = normalizeText(n, phrase)
+    gram = grams(n, tempPhrase)
+    #print(gram)
+    
+    fileExt1 = '.' + str(n) + 'gr'
+    fileExt2 = '.' + str(n-1) + 'gr'
+    language = []
+    probsArray = []
+    
+    for file in os.listdir(dirName):
+        if file.endswith(fileExt1):
+            script_dir = os.path.dirname(__file__)
+            path = os.path.join(script_dir, dirName+'/'+os.path.splitext(file)[0]+fileExt1)
+            #print(path)
+            df1 = DataFrame.from_csv(path, sep='\t', encoding='utf-8', header=None, index_col=0)
+            #df1 = Series(df1[1], index=df1.index.values)
+            #print(df1)
+            script_dir = os.path.dirname(__file__)
+            path = os.path.join(script_dir, dirName+'/'+os.path.splitext(file)[0]+fileExt2)
+            #print(path)
+            df2 = DataFrame.from_csv(path, sep='\t', encoding='utf-8', header=None, index_col=0)
+            
+            prob = 1
+            for i in gram:
+                if smoothing:
+                    #with smoothing
+                    vol = len(list(set(''.join(str(elem) for elem in df2.index.values))))
+                    temp = div(getCount(i, df1)+1, getCount(i[:-1], df2)+vol)
+                    prob *= temp
+                    #print(str(getCount(i, df1)+1), ' / ', str(getCount(i[:-1], df2)+vol), ' = ', temp)
+                else:
+                    #print(df1[[i]])
+                    try:
+                        temp = div(getCount(i, df1), getCount(i[:-1], df2))
+                        prob *= temp
+                        #print(str(getCount(i, df1)), ' / ', str(getCount(i[:-1], df2)), ' = ', temp)
+                    except:
+                        print("Unknown chars used... Please use '-s' option.")
+                        sys.exit(2)
+            
+            language.append(os.path.splitext(file)[0])
+            probsArray.append(prob)
+            
+    title = str(n)+"-gram result for \"" + phrase + "\":\n"
+    print(title)
+    testResults = DataFrame({"Language": Series(language), "Probability": Series(probsArray)})
+    testResults = testResults.set_index('Language')
+    
+    testResults = numpy.log10(testResults)
+    testResults = testResults.sort(['Probability'], ascending=False)
+    print(testResults)
+    
+    resultDescription = "\nWe estimate that " + testResults['Probability'].argmax() + " is the sentence's language.\n"
+    print(resultDescription)
+    
+    if writeOutput:
+        print("Updating file with results...")
+        tempFile = open(outFile, 'a', encoding="utf8")
+        tempFile.write(title+'\n')
+        tempFile.flush()
+        testResults.to_csv(outFile, sep='\t', encoding='utf-8', mode='a')
+        tempFile.write(resultDescription+'\n')
+        tempFile.flush()
+        tempFile.close()
+
+    if plot:
+        plt.figure();
+        testResults.plot(kind='bar');
+        plt.title(title)
+        plt.show()
 
 ### Changeable:
 #dirName = "./data"
@@ -111,16 +198,18 @@ def testSentence (n, phrase):
 #smoothing = False
 #testFile = "./test.csv"
 #fileToTest = False
+#outFile = "./resultados.txt"
+#writeOutput = False
 
 def main(argv):
    try:
-      opts, args = getopt.getopt(argv,"hd:e:n:t:c:s",["dir=", "dirName=", "ext=", "extention=", "num=", "number=", "test=", "testPhrase=", "testCSV="])
+      opts, args = getopt.getopt(argv,"hd:e:n:t:f:sow:p",["dir=", "dirName=", "ext=", "extention=", "num=", "number=", "test=", "testPhrase=", "testFile=", "writeOutputTo="])
    except getopt.GetoptError:
       print('nGram.py <options>')
       sys.exit(2)
    for opt, arg in opts:
       if opt in ('-h', "--help"):
-         print('nGram.py <options>')
+         print('nGram.py <options>\nAnd the options need to be added here...')
          # Add missing options
          sys.exit()
       elif opt in ("-d", "--dir", "--dirName"):
@@ -136,10 +225,8 @@ def main(argv):
       elif opt in ("-t", "--test", "--testPhrase"):
          global testPhrase
          testPhrase = arg
-         
-         # Needs to be refined in order to recieve a full sentence:
-         print(testPhrase)
-      elif opt in ("-c", "--testCSV"):
+         # print(testPhrase)
+      elif opt in ("-f", "--testFile"):
          global testFile
          testFile = arg
          global fileToTest
@@ -147,11 +234,38 @@ def main(argv):
       elif opt in ('-s', "--smoothing", "--smooth"):
          global smoothing
          smoothing = True
+      elif opt in ('-o', '--output'):
+         global writeOutput
+         global outFile
+         writeOutput = True
+         tempFile = open(outFile, 'w', encoding="utf8")
+         tempFile.write(str(num) + "-Gram Test Results:\n")
+         tempFile.close()
+      elif opt in ('-w', '--writeOutputTo'):
+         writeOutput = True
+         if arg != '':
+             outFile = arg
+         tempFile = open(outFile, 'w', encoding="utf8")
+         tempFile.write(str(num) + "-Gram Test Results:\n")
+         tempFile.close()
+      elif opt in ('-p', '--plot'):
+         global plot
+         plot = True
 
    normalizeFilesInDirectory (num, dirName, ext, normExt);
    createNGramFile (num, dirName, normExt);
+   createNGramFile (num-1, dirName, normExt);
    
    #Here do the testing
+   if fileToTest :
+       tempFile = open(testFile, 'r', encoding="utf8")
+       temp = tempFile.read()
+       tempFile.close()
+       temp = temp.split(';\n')
+       for i in temp:
+           testSentence(num, i)
+   else:
+       testSentence(num, testPhrase)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
