246edd6ce2821b66b5c6588fcacfd540ed23138c
Carlos Soares <cmpsoares91@gmail.com>
2014-10-27 22:52:56 +0000

    Ficheiros para entrega

---
+++ run.sh
@@ -0,0 +1,16 @@
+####################################################################
+#                           Trabalho II                            #
+####################################################################
+# Author:                                  Carlos Soares, nr 67518 #
+#                                                                  #
+# Course:           Processamento Computacional da Linguagem (NLP) #
+# Degree:                           Master in Computer Engineering #
+# University:                                        ISCTE, Lisbon #
+#                                                                  #
+# Date:                                                 27/10/2014 #
+#                                                                  #
+# File Name:                                                run.sh #
+# File Type:                                           bash script #
+#                                                                  #
+####################################################################
+
